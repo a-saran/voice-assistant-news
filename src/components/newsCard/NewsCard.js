3cedcd5d2f5fc40ade04ec60f4a88c7e33eaf30e
asaran97 <asaran234@gmail.com>
2020-08-23 00:07:49 +0530

    added news card component

--- src/components/newsCard/NewsCard.js
+++ src/components/newsCard/NewsCard.js
@@ -1,10 +1,58 @@
 import React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography
+} from "@material-ui/core";
 
-const NewsCard = () => {
+import useStyles from "./styles";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  index
+}) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>card</h1>
-    </div>
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          image={
+            urlToImage ||
+            "https://cdn.ymaws.com/www.itsmfusa.org/resource/resmgr/images/more_images/news-3.jpg"
+          }
+          className={classes.media}
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography gutterBottom variant="h5" className={classes.title}>
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography color="textSecondary" component="p" variant="body2">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <Button size="small" color="primary">
+            Learn more
+          </Button>
+          <Typography color="textSecondary" variant="h5">
+            {index + 1}
+          </Typography>
+        </CardActions>
+      </CardActionArea>
+    </Card>
   );
 };
 
--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Grid, Grow, Typography } from "@material-ui/core";
+
 import NewsCard from "../newsCard/NewsCard";
+import useStyles from "./styles";
 
 const NewsCards = ({ articles }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      {articles.map((article, i) => (
-        <NewsCard article={article} key={i} />
-      ))}
-    </div>
+    <Grow in>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
+            <NewsCard article={article} key={i} index={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 
