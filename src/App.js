ed395e08b7eca43f2b8307cca0130321d11b3cc3
asaran97 <asaran234@gmail.com>
2020-08-23 10:14:13 +0530

    added open command

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from "words-to-numbers";
 import "./App.css";
 
 import NewsCards from "./components/newsCards/NewsCards";
@@ -14,12 +15,26 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setActiveArticle(-1);
           setNewsArticles(articles);
         } else if (command === "highlight") {
           setActiveArticle(prevArticle => prevArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn.playText("Please try that again");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn.playText("opening");
+          }
         }
       }
     });
